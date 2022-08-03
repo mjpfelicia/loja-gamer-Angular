8635ae08bd1c9f9e6fa408af32a2f056d5859e8e
Sandra Felicio <mjpfelicia@gmail.com>
2022-08-03 13:09:03 -0300

    melhorando footer section icon

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $(".btn-compra").click(function () {
-        alert("Este produto não está disponível");
+        alert("Este produto não está disponível site em construção.");
     });
 
 });
